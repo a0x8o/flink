14f61c257e2fdcf87ba9f6b26a85f535d22529aa
Alex Barreto <axbarreto@lavabit.com>
2019-04-10 08:42:29 -0700

    Update japicmp comparison version to latest Flink 1.8.0

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/operations/OperationTreeBuilder.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/operations/OperationTreeBuilder.scala
@@ -18,12 +18,10 @@
 
 package org.apache.flink.table.operations
 
-import java.util.{Collections, Optional, List => JList, Map => JMap}
+import java.util.{Collections, Optional, List => JList}
 
 import org.apache.flink.api.java.operators.join.JoinType
 import org.apache.flink.table.api._
-import org.apache.flink.table.expressions.ApiExpressionUtils.valueLiteral
-import org.apache.flink.table.expressions.BuiltInFunctionDefinitions.AS
 import org.apache.flink.table.expressions.ExpressionResolver.resolverFor
 import org.apache.flink.table.expressions.FunctionDefinition.Type.SCALAR_FUNCTION
 import org.apache.flink.table.expressions._
@@ -43,9 +41,9 @@ import _root_.scala.collection.JavaConverters._
 class OperationTreeBuilder(private val tableEnv: TableEnvironment) {
 
   private val expressionBridge: ExpressionBridge[PlannerExpression] = tableEnv.expressionBridge
-  private val projectionOperationUtils = new ProjectionOperationUtils
   private val functionCatalog: FunctionDefinitionCatalog = tableEnv.functionCatalog
 
+  private val projectionOperationFactory = new ProjectionOperationFactory(expressionBridge)
   private val noWindowPropertyChecker = new NoWindowPropertyChecker(
     "Window start and end properties are not available for Over windows.")
 
@@ -99,14 +97,11 @@ class OperationTreeBuilder(private val tableEnv: TableEnvironment) {
       explicitAlias: Boolean,
       overWindows: JList[OverWindow])
     : LogicalNode = {
-
     val resolver = resolverFor(tableCatalog, functionCatalog, child).withOverWindows(overWindows)
       .build
     val projections = resolver.resolve(projectList)
-    val namedProjections = projectionOperationUtils.nameExpressions(projections)
 
-    Project(namedProjections.asScala.map(bridgeExpression), child, explicitAlias)
-      .validate(tableEnv)
+    projectionOperationFactory.create(projections, child, explicitAlias)
   }
 
   /**
@@ -150,15 +145,15 @@ class OperationTreeBuilder(private val tableEnv: TableEnvironment) {
 
   def aggregate(
       groupingExpressions: JList[Expression],
-      namedAggregates: JMap[Expression, String],
+      aggregates: JList[Expression],
       child: TableOperation)
     : TableOperation = {
 
     val childNode = child.asInstanceOf[LogicalNode]
-    val resolver = resolverFor(tableCatalog, functionCatalog, childNode).build
+    val resolver = resolverFor(tableCatalog, functionCatalog, child).build
 
     val convertedGroupings = resolveExpressions(groupingExpressions, resolver)
-    val convertedAggregates = resolveNamedExpressions(namedAggregates, resolver)
+    val convertedAggregates = resolveExpressions(aggregates, resolver)
 
     Aggregate(convertedGroupings, convertedAggregates, childNode).validate(tableEnv)
   }
@@ -170,37 +165,22 @@ class OperationTreeBuilder(private val tableEnv: TableEnvironment) {
     resolver.resolve(expressions).asScala.map(bridgeExpression)
   }
 
-  private def resolveNamedExpressions(
-      namedExpressions: JMap[Expression, String],
-      resolver: ExpressionResolver)
-    : Seq[PlannerExpression] = {
-
-    val renamedExpressions = namedExpressions.asScala
-      .map(expr => new CallExpression(AS, List(expr._1, valueLiteral(expr._2)).asJava)
-        .asInstanceOf[Expression])
-      .toList.asJava
-
-    resolver.resolve(renamedExpressions).asScala.map(bridgeExpression)
-  }
-
   def windowAggregate(
       groupingExpressions: JList[Expression],
       window: GroupWindow,
-      namedProperties: JMap[Expression, String],
-      namedAggregates: JMap[Expression, String],
+      windowProperties: JList[Expression],
+      aggregates: JList[Expression],
       child: TableOperation)
     : TableOperation = {
 
     val childNode = child.asInstanceOf[LogicalNode]
-
-    val resolver = resolverFor(tableCatalog, functionCatalog, childNode).withGroupWindow(window)
-      .build
+    val resolver = resolverFor(tableCatalog, functionCatalog, child).withGroupWindow(window).build
 
     val convertedGroupings = resolveExpressions(groupingExpressions, resolver)
 
-    val convertedAggregates = resolveNamedExpressions(namedAggregates, resolver)
+    val convertedAggregates = resolveExpressions(aggregates, resolver)
 
-    val convertedProperties = resolveNamedExpressions(namedProperties, resolver)
+    val convertedProperties = resolveExpressions(windowProperties, resolver)
 
     WindowAggregate(
         convertedGroupings,
