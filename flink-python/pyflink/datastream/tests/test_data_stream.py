bcc450457071be5b7eb8e476c11e5677f28bc3f5
Alex Barreto <axbarreto@lavabit.com>
2021-08-04 08:50:33 -0400

    [FLINK-23487][s3] Update aws and presto-hive dependencies
    Newer versions are required to make IRSA (IAM Role for Service Account)
    work properly with S3. Since presto-hive now also directly depends on
    aws-java-sdk-sts, we make sure to exclude it from presto-hive and use
    our own version instead.
    
    This fixes IRSA for s3-presto, though not yet s3-hadoop.
    
    This closes #16592.

--- flink-python/pyflink/datastream/tests/test_data_stream.py
+++ flink-python/pyflink/datastream/tests/test_data_stream.py
@@ -40,16 +40,15 @@ from pyflink.datastream.window import (CountWindowSerializer, MergingWindowAssig
                                        TimeWindowSerializer)
 from pyflink.java_gateway import get_gateway
 from pyflink.datastream.tests.test_util import DataStreamTestSinkFunction
-from pyflink.testing.test_case_utils import invoke_java_object_method, \
-    PyFlinkBatchTestCase, PyFlinkStreamingTestCase
+from pyflink.testing.test_case_utils import PyFlinkBatchTestCase, PyFlinkStreamingTestCase
+from pyflink.util.java_utils import get_j_env_configuration
 
 
 class DataStreamTests(object):
 
     def setUp(self) -> None:
         super(DataStreamTests, self).setUp()
-        config = invoke_java_object_method(
-            self.env._j_stream_execution_environment, "getConfiguration")
+        config = get_j_env_configuration(self.env._j_stream_execution_environment)
         config.setString("akka.ask.timeout", "20 s")
         self.test_sink = DataStreamTestSinkFunction()
 
@@ -561,7 +560,7 @@ class DataStreamTests(object):
                                       type_info=Types.ROW([Types.STRING(), Types.INT()]))
         ds.print()
         plan = eval(str(self.env.get_execution_plan()))
-        self.assertEqual("Sink: Print to Std. Out", plan['nodes'][1]['type'])
+        self.assertEqual("Sink: Print to Std. Out", plan['nodes'][2]['type'])
 
     def test_print_with_align_output(self):
         # need to align output type before print, therefore the plan will contain three nodes
