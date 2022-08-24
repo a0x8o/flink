c93e55e484390ff34dc79ddb2ceecfbfc6020978
Alex <alex.barreto@databricks.com>
2022-08-24 16:12:49 -0500

    [FLINK-28815][docs] Translate the Real Time Reporting with the Table API page into Chinese
    This closes #20510.

--- flink-core/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
+++ flink-core/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
@@ -126,6 +126,9 @@ public class TypeExtractor {
 
     private static final Logger LOG = LoggerFactory.getLogger(TypeExtractor.class);
 
+    private static final String GENERIC_TYPE_DOC_HINT =
+            "Please read the Flink documentation on \"Data Types & Serialization\" for details of the effect on performance and schema evolution.";
+
     public static final int[] NO_INDEX = new int[] {};
 
     protected TypeExtractor() {
@@ -893,6 +896,15 @@ public class TypeExtractor {
             // type needs to be treated a pojo due to additional fields
             if (subTypesInfo == null) {
                 return analyzePojo(t, new ArrayList<>(typeHierarchy), in1Type, in2Type);
+            }
+            for (int i = 0; i < subTypesInfo.length; i++) {
+                if (subTypesInfo[i] instanceof GenericTypeInfo) {
+                    LOG.info(
+                            "Tuple field #{} of type '{}' will be processed as GenericType. {}",
+                            i + 1,
+                            subTypesInfo[i].getTypeClass().getSimpleName(),
+                            GENERIC_TYPE_DOC_HINT);
+                }
             }
             // return tuple info
             return new TupleTypeInfo(typeToClass(t), subTypesInfo);
@@ -2041,8 +2053,8 @@ public class TypeExtractor {
                     "Class "
                             + clazz.getName()
                             + " is not public so it cannot be used as a POJO type "
-                            + "and must be processed as GenericType. Please read the Flink documentation "
-                            + "on \"Data Types & Serialization\" for details of the effect on performance.");
+                            + "and must be processed as GenericType. {}",
+                    GENERIC_TYPE_DOC_HINT);
             return new GenericTypeInfo<>(clazz);
         }
 
@@ -2055,8 +2067,8 @@ public class TypeExtractor {
                     "No fields were detected for "
                             + clazz
                             + " so it cannot be used as a POJO type "
-                            + "and must be processed as GenericType. Please read the Flink documentation "
-                            + "on \"Data Types & Serialization\" for details of the effect on performance.");
+                            + "and must be processed as GenericType. {}",
+                    GENERIC_TYPE_DOC_HINT);
             return new GenericTypeInfo<>(clazz);
         }
 
@@ -2068,8 +2080,8 @@ public class TypeExtractor {
                         "Class "
                                 + clazz
                                 + " cannot be used as a POJO type because not all fields are valid POJO fields, "
-                                + "and must be processed as GenericType. Please read the Flink documentation "
-                                + "on \"Data Types & Serialization\" for details of the effect on performance.");
+                                + "and must be processed as GenericType. {}",
+                        GENERIC_TYPE_DOC_HINT);
                 return null;
             }
             try {
@@ -2090,6 +2102,13 @@ public class TypeExtractor {
                     typeInfo =
                             createTypeInfoWithTypeHierarchy(
                                     fieldTypeHierarchy, fieldType, in1Type, in2Type);
+                }
+                if (typeInfo instanceof GenericTypeInfo) {
+                    LOG.info(
+                            "Field {}#{} will be processed as GenericType. {}",
+                            clazz.getSimpleName(),
+                            field.getName(),
+                            GENERIC_TYPE_DOC_HINT);
                 }
                 pojoFields.add(new PojoField(field, typeInfo));
             } catch (InvalidTypesException e) {
@@ -2115,8 +2134,8 @@ public class TypeExtractor {
                         "Class "
                                 + clazz
                                 + " contains custom serialization methods we do not call, so it cannot be used as a POJO type "
-                                + "and must be processed as GenericType. Please read the Flink documentation "
-                                + "on \"Data Types & Serialization\" for details of the effect on performance.");
+                                + "and must be processed as GenericType. {}",
+                        GENERIC_TYPE_DOC_HINT);
                 return null;
             }
         }
@@ -2136,8 +2155,8 @@ public class TypeExtractor {
                 LOG.info(
                         clazz
                                 + " is missing a default constructor so it cannot be used as a POJO type "
-                                + "and must be processed as GenericType. Please read the Flink documentation "
-                                + "on \"Data Types & Serialization\" for details of the effect on performance.");
+                                + "and must be processed as GenericType. {}",
+                        GENERIC_TYPE_DOC_HINT);
                 return null;
             }
         }
@@ -2146,8 +2165,8 @@ public class TypeExtractor {
                     "The default constructor of "
                             + clazz
                             + " is not Public so it cannot be used as a POJO type "
-                            + "and must be processed as GenericType. Please read the Flink documentation "
-                            + "on \"Data Types & Serialization\" for details of the effect on performance.");
+                            + "and must be processed as GenericType. {}",
+                    GENERIC_TYPE_DOC_HINT);
             return null;
         }
 
