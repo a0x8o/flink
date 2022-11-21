b364e2cd85dc82dd12f10197d8c0f0bca84e3ec0
Alex <alex.barreto@databricks.com>
2022-11-21 08:26:24 -0600

    [FLINK-30085][yarn][tests] Lower client timeouts

--- flink-runtime/src/test/java/org/apache/flink/runtime/io/network/netty/PartitionRequestClientFactoryTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/netty/PartitionRequestClientFactoryTest.java
@@ -27,9 +27,12 @@ import org.apache.flink.testutils.junit.extensions.parameterized.ParameterizedTe
 import org.apache.flink.testutils.junit.extensions.parameterized.Parameters;
 import org.apache.flink.util.TestLogger;
 
+import org.apache.flink.shaded.netty4.io.netty.channel.Channel;
 import org.apache.flink.shaded.netty4.io.netty.channel.ChannelException;
 import org.apache.flink.shaded.netty4.io.netty.channel.ChannelFuture;
 import org.apache.flink.shaded.netty4.io.netty.channel.ChannelHandler;
+import org.apache.flink.shaded.netty4.io.netty.channel.ChannelHandlerContext;
+import org.apache.flink.shaded.netty4.io.netty.channel.ChannelInboundHandlerAdapter;
 
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -48,6 +51,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import static org.apache.flink.runtime.io.network.netty.NettyTestUtil.initServerAndClient;
+import static org.apache.flink.runtime.io.network.netty.NettyTestUtil.shutdown;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -81,8 +86,7 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
             factory.createPartitionRequestClient(
                     nettyServerAndClient.getConnectionID(RESOURCE_ID, 0));
         } finally {
-            nettyServerAndClient.client().shutdown();
-            nettyServerAndClient.server().shutdown();
+            shutdown(nettyServerAndClient);
         }
     }
 
@@ -125,8 +129,7 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
 
             factory.createPartitionRequestClient(connectionID);
         } finally {
-            nettyServerAndClient.client().shutdown();
-            nettyServerAndClient.server().shutdown();
+            shutdown(nettyServerAndClient);
         }
     }
 
@@ -139,8 +142,7 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
             checkReuseNettyPartitionRequestClient(nettyServerAndClient, 5);
             checkReuseNettyPartitionRequestClient(nettyServerAndClient, 10);
         } finally {
-            nettyServerAndClient.client().shutdown();
-            nettyServerAndClient.server().shutdown();
+            shutdown(nettyServerAndClient);
         }
     }
 
@@ -164,6 +166,56 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
         assertThat(set.size()).isLessThanOrEqualTo(maxNumberOfConnections);
     }
 
+    /**
+     * Verify that the netty client reuse when the netty server closes the channel and there is no
+     * input channel.
+     */
+    @TestTemplate
+    void testConnectionReuseWhenRemoteCloseAndNoInputChannel() throws Exception {
+        CompletableFuture<Void> inactiveFuture = new CompletableFuture<>();
+        CompletableFuture<Channel> serverChannelFuture = new CompletableFuture<>();
+        NettyProtocol protocol =
+                new NettyProtocol(null, null) {
+                    @Override
+                    public ChannelHandler[] getServerChannelHandlers() {
+                        return new ChannelHandler[] {
+                            // Close on read
+                            new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelRegistered(ChannelHandlerContext ctx)
+                                        throws Exception {
+                                    super.channelRegistered(ctx);
+                                    serverChannelFuture.complete(ctx.channel());
+                                }
+                            }
+                        };
+                    }
+
+                    @Override
+                    public ChannelHandler[] getClientChannelHandlers() {
+                        return new ChannelHandler[] {
+                            new ChannelInactiveFutureHandler(inactiveFuture)
+                        };
+                    }
+                };
+        NettyTestUtil.NettyServerAndClient serverAndClient = initServerAndClient(protocol);
+
+        PartitionRequestClientFactory factory =
+                new PartitionRequestClientFactory(
+                        serverAndClient.client(), 2, 1, connectionReuseEnabled);
+
+        ConnectionID connectionID = serverAndClient.getConnectionID(RESOURCE_ID, 0);
+        NettyPartitionRequestClient oldClient = factory.createPartitionRequestClient(connectionID);
+
+        // close server channel
+        Channel channel = serverChannelFuture.get();
+        channel.close();
+        inactiveFuture.get();
+        NettyPartitionRequestClient newClient = factory.createPartitionRequestClient(connectionID);
+        assertThat(newClient).as("Factory should create a new client.").isNotSameAs(oldClient);
+        shutdown(serverAndClient);
+    }
+
     @TestTemplate
     void testNettyClientConnectRetry() throws Exception {
         NettyTestUtil.NettyServerAndClient serverAndClient = createNettyServerAndClient();
@@ -176,13 +228,12 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
 
         factory.createPartitionRequestClient(serverAndClient.getConnectionID(RESOURCE_ID, 0));
 
-        serverAndClient.client().shutdown();
-        serverAndClient.server().shutdown();
+        shutdown(serverAndClient);
     }
 
     // see https://issues.apache.org/jira/browse/FLINK-18821
     @TestTemplate
-    void testFailureReportedToSubsequentRequests() throws Exception {
+    void testFailureReportedToSubsequentRequests() {
         PartitionRequestClientFactory factory =
                 new PartitionRequestClientFactory(
                         new FailingNettyClient(), 2, 1, connectionReuseEnabled);
@@ -218,14 +269,12 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
                             unstableNettyClient, 2, 1, connectionReuseEnabled);
 
             assertThatThrownBy(
-                            () -> {
-                                factory.createPartitionRequestClient(
-                                        serverAndClient.getConnectionID(RESOURCE_ID, 0));
-                            })
+                            () ->
+                                    factory.createPartitionRequestClient(
+                                            serverAndClient.getConnectionID(RESOURCE_ID, 0)))
                     .isInstanceOf(IOException.class);
         } finally {
-            serverAndClient.client().shutdown();
-            serverAndClient.server().shutdown();
+            shutdown(serverAndClient);
         }
     }
 
@@ -274,8 +323,7 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
                 });
 
         threadPoolExecutor.shutdown();
-        serverAndClient.client().shutdown();
-        serverAndClient.server().shutdown();
+        shutdown(serverAndClient);
     }
 
     private NettyTestUtil.NettyServerAndClient createNettyServerAndClient() throws Exception {
@@ -348,6 +396,26 @@ public class PartitionRequestClientFactoryTest extends TestLogger {
             } catch (Exception exception) {
                 throw new RuntimeException(exception);
             }
+        }
+    }
+
+    private static class ChannelInactiveFutureHandler
+            extends CreditBasedPartitionRequestClientHandler {
+
+        private final CompletableFuture<Void> inactiveFuture;
+
+        private ChannelInactiveFutureHandler(CompletableFuture<Void> inactiveFuture) {
+            this.inactiveFuture = inactiveFuture;
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            super.channelInactive(ctx);
+            inactiveFuture.complete(null);
+        }
+
+        public CompletableFuture<Void> getInactiveFuture() {
+            return inactiveFuture;
         }
     }
 }
