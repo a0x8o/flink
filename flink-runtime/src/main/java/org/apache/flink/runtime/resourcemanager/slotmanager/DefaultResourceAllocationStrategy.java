9dc884c5b135a6c7f0a68504d6e6f3e6cf460356
Alexandre Barreto <abarre25@jh.edu>
2023-10-11 13:59:47 -0400

    [FLINK-33166][core][config] Make root logger level configurable (#23476)
    * [FLINK-33166][core][config] Make root logger level configurable

--- flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/slotmanager/DefaultResourceAllocationStrategy.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/slotmanager/DefaultResourceAllocationStrategy.java
@@ -53,13 +53,13 @@ import static org.apache.flink.runtime.resourcemanager.slotmanager.SlotManagerUt
  * resources and cannot fit into the pre-defined total resource profile.
  *
  * <p>Note: This strategy tries to find a feasible allocation result, rather than an optimal one (in
- * term of resource utilization). It also does not guarantee always finding a feasible solution when
- * exist.
+ * terms of resource utilization). It also does not guarantee always finding a feasible solution
+ * when exists.
  *
  * <p>Note: The current implementation of this strategy is non-optimal, in terms of computation
  * efficiency. In the worst case, for each distinctly profiled requirement it checks all registered
  * and pending resources. Further optimization requires complex data structures for ordering
- * multi-dimensional resource profiles. The complexity is not necessary.
+ * multidimensional resource profiles. The complexity is not necessary.
  */
 public class DefaultResourceAllocationStrategy implements ResourceAllocationStrategy {
     private final ResourceProfile defaultSlotResourceProfile;
@@ -71,7 +71,7 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
 
     /**
      * Always use any matching strategy for pending resources to use as less pending workers as
-     * possible, so that the rest can be canceled
+     * possible, so that the rest can be canceled.
      */
     private final ResourceMatchingStrategy pendingResourceMatchingStrategy =
             AnyMatchingResourceMatchingStrategy.INSTANCE;
@@ -131,12 +131,13 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
                             jobId, resourceRequirements.getValue(), registeredResources);
 
             if (!unfulfilledJobRequirements.isEmpty()) {
-                tryFulfillRequirementsForJobWithPendingResources(
-                        jobId,
-                        unfulfilledJobRequirements,
-                        pendingResources,
-                        totalCurrentResources,
-                        resultBuilder);
+                totalCurrentResources =
+                        totalCurrentResources.merge(
+                                tryFulfillRequirementsForJobWithPendingResources(
+                                        jobId,
+                                        unfulfilledJobRequirements,
+                                        pendingResources,
+                                        resultBuilder));
             }
         }
 
@@ -144,7 +145,7 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
         // to the latest state after a new PendingTaskManager is created,
         // tryFulFillRequiredResources will not update pendingResources even after new
         // PendingTaskManagers are created.
-        // This is because the pendingResources are no longer needed afterwards.
+        // This is because the pendingResources are no longer needed afterward.
         tryFulFillRequiredResources(
                 registeredResources, pendingResources, totalCurrentResources, resultBuilder);
         return resultBuilder.build();
@@ -311,12 +312,13 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
         return resourceProfile.allFieldsNoLessThan(requirement);
     }
 
-    private void tryFulfillRequirementsForJobWithPendingResources(
+    private ResourceProfile tryFulfillRequirementsForJobWithPendingResources(
             JobID jobId,
             Collection<ResourceRequirement> unfulfilledRequirements,
             List<InternalResourceInfo> availableResources,
-            ResourceProfile totalCurrentResources,
             ResourceAllocationResult.Builder resultBuilder) {
+        ResourceProfile newAddedResourceProfile = ResourceProfile.ZERO;
+
         for (ResourceRequirement missingResource : unfulfilledRequirements) {
             // for this strategy, all pending resources should have the same default slot resource
             final ResourceProfile effectiveProfile =
@@ -334,13 +336,12 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
                 resultBuilder.addUnfulfillableJob(jobId);
                 continue;
             }
-
             while (numUnfulfilled > 0) {
                 // Circularly add new pending task manager
                 final PendingTaskManager newPendingTaskManager =
                         new PendingTaskManager(totalResourceProfile, numSlotsPerWorker);
                 resultBuilder.addPendingTaskManagerAllocate(newPendingTaskManager);
-                totalCurrentResources.merge(totalResourceProfile);
+                newAddedResourceProfile = newAddedResourceProfile.merge(totalResourceProfile);
                 ResourceProfile remainResource = totalResourceProfile;
                 while (numUnfulfilled > 0
                         && canFulfillRequirement(effectiveProfile, remainResource)) {
@@ -365,6 +366,8 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
                 }
             }
         }
+
+        return newAddedResourceProfile;
     }
 
     private boolean isRequiredResourcesFulfilled(
@@ -386,9 +389,9 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
     private void tryFulFillRequiredResources(
             List<InternalResourceInfo> availableRegisteredResources,
             List<InternalResourceInfo> availablePendingResources,
-            ResourceProfile totalCurrentResources,
+            ResourceProfile resourcesInTotal,
             ResourceAllocationResult.Builder resultBuilder) {
-        ResourceProfile totalAvailableResources =
+        ResourceProfile resourcesAvailable =
                 Stream.concat(
                                 availableRegisteredResources.stream(),
                                 availablePendingResources.stream())
@@ -396,9 +399,7 @@ public class DefaultResourceAllocationStrategy implements ResourceAllocationStra
                         .reduce(ResourceProfile.ZERO, ResourceProfile::merge);
 
         tryFulFillRequiredResourcesWithAction(
-                totalAvailableResources,
-                totalCurrentResources,
-                resultBuilder::addPendingTaskManagerAllocate);
+                resourcesAvailable, resourcesInTotal, resultBuilder::addPendingTaskManagerAllocate);
     }
 
     private void tryFulFillRequiredResourcesWithAction(
